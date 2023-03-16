c200feb70a833477143d8165e2cfb8ee4b7bfacf
Kamaleshen S <kamaleshen.s@gmail.com>
2023-03-16 12:42:31 +0530

    fix: fix issues in auth services

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,30 +1,30 @@
-const {User} = require('../models');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { User } = require('../models');
 
 const createUser = (username, password) => {
-  const user = User.findOne({where: {username}})
-  if(user) {
+  const user = User.findOne({ where: { username } });
+  if (user) {
     throw new Error('user already exists');
   }
-  password = bcrypt.hashSync(password, 10);
-  const result = User.create({username, password});
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  const result = User.create({ username, hashedPassword });
   return result;
-}
+};
 
 const login = async (username, password) => {
   const result = await User.findOne({ where: { username } });
-  if(!result) {
+  if (!result) {
     throw new Error('user not found');
   }
   const comparePassword = bcrypt.compare(password, result.password);
-  if(!comparePassword) {
+  if (!comparePassword) {
     throw new Error('wrong password');
   }
 
-  const token = jwt.sign({ username: username}, 'secret', { expiresIn: '1d' });
+  const token = jwt.sign({ username }, 'secret', { expiresIn: '1d' });
   return token;
-}
+};
 
 module.exports = {
   createUser,
