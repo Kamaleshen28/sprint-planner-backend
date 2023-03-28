e2944b3e9d8699cfa891bc83dc16f7be2f60518b
pranav_vohra <pranavvohra1812@yahoo.in>
2023-03-28 13:03:31 +0530

    feat: send start and end dates in csv

--- src/utils/projects/csvConversion.js
+++ src/utils/projects/csvConversion.js
@@ -1,7 +1,40 @@
+const returnStartAndEndDates = (projectStartDate, startDay, endDay) => {
+  let startDate = new Date(projectStartDate);
+  let count = 0;
+  while (
+    count < startDay ||
+    startDate.getDay() === 0 ||
+    startDate.getDay() === 6
+  ) {
+    startDate = new Date(startDate.setDate(startDate.getDate() + 1));
+    if (startDate.getDay() !== 0 && startDate.getDay() !== 6) {
+      // Date.getDay() gives weekday starting from 0(Sunday) to 6(Saturday)
+      count++;
+    }
+  }
+
+  const duration = endDay - startDay - 1;
+  let endDate = new Date(startDate);
+
+  count = 0;
+  while (count < duration) {
+    endDate = new Date(endDate.setDate(endDate.getDate() + 1));
+    if (endDate.getDay() !== 0 && endDate.getDay() !== 6) {
+      // Date.getDay() gives weekday starting from 0(Sunday) to 6(Saturday)
+      count++;
+    }
+  }
+  return {
+    startDate,
+    endDate,
+  };
+};
+
 const csvConversion = (data) => {
-  const { plannedStories, developers } = data;
+  const { plannedStories, developers, projectStartDate } = data;
   const dataToConvert = plannedStories.map((story, ind) => ({
     index: ind + 1,
+    ...returnStartAndEndDates(projectStartDate, story.startDay, story.endDay),
     title: story.title,
     // description: story.description,
     developer: developers[story.assignedDeveloperId].name,
