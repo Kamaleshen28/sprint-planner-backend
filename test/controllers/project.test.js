910b2cb3445047ce6e6c6e80e089e883cf706ace
GauravPoosarla <poosarlagaurav@gmail.com>
2023-04-04 11:55:45 +0530

    feat: add tests for getProject controller function

--- test/controllers/project.test.js
+++ test/controllers/project.test.js
@@ -3,114 +3,122 @@ const projectController = require('../../src/controllers/projects');
 const projectUtils = require('../../src/utils/projects');
 
 describe('Project Controller', () => {
-  describe('createProject', () => {
-    it('should return 500 if projectServices.createProject throws an error', async () => {
-      const mockReq = {
-        body: {
-          title: 'Test Project',
-          sprintDuration: 2,
-          stories: [],
-          developers: [],
-        },
-      };
-      const mockRes = {
-        status: jest.fn().mockReturnThis(),
-        json: jest.fn(),
-      };
-      jest
-        .spyOn(projectServices, 'createProject')
-        .mockRejectedValue(new Error('Test Error'));
-      await projectController.createProject(mockReq, mockRes);
-      expect(mockRes.status).toBeCalledWith(500);
-      expect(mockRes.json).toBeCalledWith({ message: 'Test Error' });
-    });
-
-    it('should return 201 if projectServices.createProject returns a project', async () => {
-      const mockReq = {
-        body: {
-          title: 'Test Project',
-          sprintDuration: 2,
-          stories: [],
-          developers: [],
-        },
-      };
-      const mockRes = {
-        status: jest.fn().mockReturnThis(),
-        json: jest.fn(),
-      };
-      const mockResult = { id: 1, title: 'Test Project' };
-      jest
-        .spyOn(projectServices, 'createProject')
-        .mockResolvedValue(mockResult);
-      const mockData = {
-        title: 'Test Project',
-        sprintDuration: 2,
-        stories: [],
-        developers: [],
-      };
-      jest.spyOn(projectUtils, 'calculateSprint').mockReturnValue(mockData);
-      await projectController.createProject(mockReq, mockRes);
-      expect(mockRes.status).toBeCalledWith(201);
-      expect(mockRes.json).toBeCalledWith({
-        message: 'Project created successfully',
-        data: mockData,
-      });
-    });
-  });
-
   describe('getProject', () => {
-    it('should return 500 if projectServices.getProject throws an error', async () => {
-      const mockReq = {
+    it('should return 404 if project not found', async () => {
+      const req = {
         params: {
-          id: 1,
+          id: '123',
+        },
+        user: {
+          username: 'test',
         },
       };
-      const mockRes = {
+      const res = {
         status: jest.fn().mockReturnThis(),
         json: jest.fn(),
       };
-      jest
-        .spyOn(projectServices, 'getProject')
-        .mockRejectedValue(new Error('Test Error'));
-      await projectController.getProject(mockReq, mockRes);
-      expect(mockRes.status).toBeCalledWith(500);
-      expect(mockRes.json).toBeCalledWith({ message: 'Test Error' });
+      projectServices.getProject = jest.fn().mockReturnValue(null);
+      await projectController.getProject(req, res);
+      expect(res.status).toHaveBeenCalledWith(404);
+      expect(res.json).toHaveBeenCalledWith({ message: 'Project not found' });
     });
 
-    it('should return 404 if projectServices.getProject returns null', async () => {
-      const mockReq = {
+    it('should return 200 if project found', async () => {
+      const req = {
         params: {
-          id: 1,
+          id: '123',
+        },
+        user: {
+          username: 'test',
         },
       };
-      const mockRes = {
+      const res = {
         status: jest.fn().mockReturnThis(),
         json: jest.fn(),
       };
-      jest.spyOn(projectServices, 'getProject').mockResolvedValue(null);
-      await projectController.getProject(mockReq, mockRes);
-      expect(mockRes.status).toBeCalledWith(404);
-      expect(mockRes.json).toBeCalledWith({ message: 'Project not found' });
-    });
-
-    it('should return 200 if projectServices.getProject returns a project', async () => {
-      const mockReq = {
-        params: {
-          id: 1,
-        },
-      };
-      const mockRes = {
-        status: jest.fn().mockReturnThis(),
-        json: jest.fn(),
-      };
-      const mockResult = { id: 1, title: 'Test Project' };
-      jest.spyOn(projectServices, 'getProject').mockResolvedValue(mockResult);
-      await projectController.getProject(mockReq, mockRes);
-      expect(mockRes.status).toBeCalledWith(200);
-      expect(mockRes.json).toBeCalledWith({
+      projectServices.getProject = jest.fn().mockReturnValue({
+        status: 'planned',
         message: 'Project fetched successfully',
-        data: mockResult,
+      });
+
+      projectUtils.calculateSprint = jest.fn().mockReturnValue({
+        status: 'planned',
+        message: 'Project fetched successfully',
+      });
+
+      await projectController.getProject(req, res);
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.json).toHaveBeenCalledWith({
+        message: 'Project fetched successfully',
+        data: {
+          status: 'planned',
+          message: 'Project fetched successfully',
+        },
       });
     });
+
+    it('should return 200 if project found and status is unsupportedInput', async () => {
+      const req = {
+        params: {
+          id: '123',
+        },
+        user: {
+          username: 'test',
+        },
+      };
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      projectServices.getProject = jest.fn().mockReturnValue({
+        status: 'unsupportedInput',
+        message: 'Project Draft',
+      });
+
+      projectUtils.calculateSprint = jest.fn().mockReturnValue({
+        status: 'unsupportedInput',
+        message: 'Project Draft',
+      });
+
+      await projectController.getProject(req, res);
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.json).toHaveBeenCalledWith({
+        message: 'Project Draft',
+        data: {
+          status: 'unsupportedInput',
+          message: 'Project Draft',
+        },
+      });
+    });
+
+    it('should return 500 if error is thrown', async () => {
+      const req = {
+        params: {
+          id: '123',
+        },
+        user: {
+          username: 'test',
+        },
+      };
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      projectServices.getProject = jest.fn().mockImplementation(() => {
+        throw new Error('Error');
+      });
+
+      projectUtils.calculateSprint = jest.fn().mockReturnValue({
+        status: 'unsupportedInput',
+        message: 'Project Draft',
+      });
+
+      await projectController.getProject(req, res);
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.json).toHaveBeenCalledWith({ message: 'Error' });
+    });
   });
+
+  describe('downloadAsCSV', () => {
+    
 });
