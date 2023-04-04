e48dbfb401efdab7349edf26bace2071a6abda36
GauravPoosarla <poosarlagaurav@gmail.com>
2023-04-04 22:47:45 +0530

    feat: add tests for params validator

--- test/middlewares/validate.test.js
+++ test/middlewares/validate.test.js
@@ -38,4 +38,39 @@ describe('validate middleware', () => {
       });
     });
   });
+
+  describe('validateParams', () => {
+    it('should validate params', () => {
+      const req = {
+        params: {
+          id: '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed',
+        },
+      };
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      const next = jest.fn();
+      validateParams(validationSchemas.getProjectParams)(req, res, next);
+      expect(next).toHaveBeenCalled();
+    });
+
+    it('should throw error if params is invalid', () => {
+      const req = {
+        params: {
+          id: '123',
+        },
+      };
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      const next = jest.fn();
+      validateParams(validationSchemas.getProjectParams)(req, res, next);
+      expect(res.status).toHaveBeenCalledWith(400);
+      expect(res.json).toHaveBeenCalledWith({
+        message: '"id" must be a valid GUID',
+      });
+    });
+  });
 });
